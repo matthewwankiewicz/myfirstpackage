d4640fdf73773b502ca7339bebf50c6f8449843b
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2021-05-01 14:34:56 -0400

    first commit

---
+++ R/seven_times.R
@@ -0,0 +1,15 @@
+#' @title Seven Times
+#'
+#' @description This gives seven times a number.
+#'
+#' @param integer
+#'
+#' @return NULL
+#'
+#' @examples  seven_times(3)
+#'
+#' @export seven_times
+
+seven_times <- function(integer) {
+  7*integer
+}
